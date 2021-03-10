c1124f83d54a317ad2075dab59197929aa90b2dd
Mario Navarro <mario.navarro93@gmail.com>
2021-03-10 12:44:39 +0000

    Added and checked the endpoints

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,12 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
+    /* Get all the categories of DB */
     public function getAll() {
-        return "hola";
+        return response()->json(Category::all(), 200);
+    }
+
+    /* Get the info of one category */
+    public function getCategory($id) {
+        $category = Category::find($id);
+        $code = 200;
+
+        if(!$category) 
+            $code = 404;
+
+        return response()->json($category, $code);
+    }
+
+    /* Add a new category */
+    public function saveCategory(Request $request) {
+        $category = Category::create(array(
+            'name' => $request->name
+        ));
+        $category->save();
+        return response()->json("The category has been created", 200);
+    }
+
+    /* Edit a category */
+    public function editCategory($id, Request $request) {
+        $category = Category::find($id);
+        $category->name = $request->name;
+        $category->save();
+    }
+
+    /* Delete a category (Logical deletion) */
+    public function deleteCategory($id) {
+        $category = Category::find($id);
+        $category->delete();
     }
 }
--- routes/api.category.php
+++ routes/api.category.php
@@ -11,6 +11,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'welcome';
-});
+Route::get('/', 'CategoryController@getAll');
+
+Route::get('/{id}', 'CategoryController@getCategory');
+
+Route::delete('/{id}', 'CategoryController@deleteCategory');
+
+Route::post('/', 'CategoryController@saveCategory');
+
+Route::put('/{id}', 'CategoryController@editCategory');
